ef6e8d4cc5cca7e3a5e44728b19e608a6a6469f6
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-08-15 08:20:41 -0700

    Updated examples to be more comprehensive

--- examples/st7735r_simpletest.py
+++ examples/st7735r_simpletest.py
@@ -1,10 +1,12 @@
 """
-This test will initialize the display using displayio
-and draw a solid red background
+This test will initialize the display using displayio and draw a solid green
+background, a smaller purple rectangle, and some yellow text.
 """
 
 import board
 import displayio
+import terminalio
+from adafruit_display_text import label
 from adafruit_st7735r import ST7735R
 
 spi = board.SPI()
@@ -22,12 +24,26 @@ display.show(splash)
 
 color_bitmap = displayio.Bitmap(128, 128, 1)
 color_palette = displayio.Palette(1)
-color_palette[0] = 0xFF0000
+color_palette[0] = 0x00FF00 # Bright Green
 
 bg_sprite = displayio.TileGrid(color_bitmap,
                                pixel_shader=color_palette,
                                x=0, y=0)
 splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle
+inner_bitmap = displayio.Bitmap(108, 108, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0xAA0088 # Purple
+inner_sprite = displayio.TileGrid(inner_bitmap,
+                                  pixel_shader=inner_palette,
+                                  x=10, y=10)
+splash.append(inner_sprite)
+
+# Draw a label
+text = "Hello World!"
+text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=30, y=64)
+splash.append(text_area)
 
 while True:
     pass
